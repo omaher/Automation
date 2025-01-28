99ecd0355cf4a9877158708c1443be53707e096f
Omkar Aher <aher.omkar.15et7021@gmail.com>
2025-01-28 23:38:38 +0530

    Added Options package

--- automate_naukri.py
+++ automate_naukri.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import os
 
